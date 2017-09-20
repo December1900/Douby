dda3e4a2acefe38d18dab5d4416ede8eebadb926
December1900 <522376693@qq.com>
2017-09-20 10:34:47 +0800

    * revise cache interceptor

--- app/src/main/java/net/december1900/douby/net/CacheInterceptor.java
+++ app/src/main/java/net/december1900/douby/net/CacheInterceptor.java
@@ -1,10 +1,12 @@
 package net.december1900.douby.net;
 
-import android.text.TextUtils;
 import android.util.Log;
+
+import net.december1900.douby.util.NetUtil;
 
 import java.io.IOException;
 
+import okhttp3.CacheControl;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -19,47 +21,34 @@ public class CacheInterceptor implements Interceptor {
 
     @Override
     public Response intercept(Chain chain) throws IOException {
-//        Request request = chain.request();
-//        if (!NetUtil.isConnected()) {
-//            request = request.newBuilder()
-//                    .cacheControl(CacheControl.FORCE_CACHE)
-//                    .build();
-//            Log.d(TAG, "no network");
-//        }
-//
-//        Response response = chain.proceed(request);
-//        if (NetUtil.isConnected()) {
-//            int maxAge = 2 * 60;
-//            Log.d(TAG, "android has maxAge :" + maxAge);
-//            response.newBuilder()
-//                    .header("Cache-Control", "public ,max-age=" + maxAge)
-//                    .removeHeader("Pragma")
-//                    .build();
-//        } else {
-//            Log.d(TAG, "no network");
-//            int maxStale = 60 * 60 * 24 * 28;
-//            response.newBuilder()
-//                    .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
-//                    .removeHeader("Pragma)
-//                    .build();
-//            Log.d(TAG, "android has maxStale :" + maxStale);
-//
-//        }
-
         Request request = chain.request();
-        Log.i(TAG, "request=" + request);
-        Response response = chain.proceed(request);
-        Log.i(TAG, "response=" + response);
-
-        String cacheControl = request.cacheControl().toString();
-        if (TextUtils.isEmpty(cacheControl)) {
-            cacheControl = "public, max-age=120";
+        if (!NetUtil.isConnected()) {
+            request = request.newBuilder()
+                    .cacheControl(CacheControl.FORCE_CACHE)
+                    .build();
+            Log.d(TAG, "no network");
         }
-        return response.newBuilder()
-                .header("Cache-Control", cacheControl)
-                .removeHeader("Pragma")
-                .build();
 
+        Response response = chain.proceed(request);
+        if (NetUtil.isConnected()) {
+            int maxAge = 2 * 60;
+            Log.d(TAG, "android has maxAge :" + maxAge);
+            response.newBuilder()
+                    .header("Cache-Control", "public ,max-age=" + maxAge)
+                    .removeHeader("Pragma")
+                    .build();
+        } else {
+            Log.d(TAG, "no network");
+            int maxStale = 60 * 60 * 24 * 28;
+            response.newBuilder()
+                    .header("Cache-Control", "public, only-if-cached, max-stale=" + maxStale)
+                    .removeHeader("Pragma")
+                    .build();
+            Log.d(TAG, "android has maxStale :" + maxStale);
+
+        }
+
+        return response;
 
     }
 }
